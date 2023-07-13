b96f57e62400c2c521a690b735de560512cefec3
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-13 17:52:37 +0530

    [Yuvanthika] Add : Implemented

---
+++ BasicPrograms/LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class LeapYear
+    {
+        public void CheckLeapYear()
+        {
+            Console.WriteLine("Enter the year : ");
+            int year = Convert.ToInt32(Console.ReadLine()); 
+            if((year %4 ==0) && (year %100 != 0) || (year % 400)==0))
+                    Console.WriteLine("Leap year");
+           else
+                Console.WriteLine("Not a Leap year");
+        }
+    }
+}
