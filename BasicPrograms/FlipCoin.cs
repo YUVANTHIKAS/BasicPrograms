b807f7f176fe1e6ef07cd579c9c31b0a10c3d57a
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-13 17:34:23 +0530

    [Yuvanthika] Add : Implemented

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public  class FlipCoin
+    {
+        public void Play()
+        {
+            Random random = new Random();
+            Console.WriteLine("Enter no of times the coin need to be flipped");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int headCount = 0, tailCount = 0;
+            for(int i=0;i<num;i++)
+            {
+                double check = random.NextDouble();
+                if (check < 0.5)
+                    tailCount++;
+                else
+                    headCount++;
+
+
+            }
+            Console.WriteLine("percentage of Head" + " " + (headCount / num) * 100);
+            Console.WriteLine("percentage of tail" + " " + (tailCount / num) * 100);
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BasicPrograms
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin flipCoin = new FlipCoin();
+            flipCoin.Play();
+        }
+    }
+}
